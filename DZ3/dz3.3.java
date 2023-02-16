165ee52d2e19e1a99a711229938e9c5226f9d3fe
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-02-16 15:47:05 +0000

    Update and rename Main (6).java to dz3.3.java

--- DZ3/dz3.3.java
+++ DZ3/dz3.3.java
@@ -34,41 +34,6 @@ public class Main {
         System.out.println("Місячна зарплата без податків " + salary);
         System.out.println("Місячна зарплата з податками " + (salary - (salary * rent / 100)));
 
-       /* String days [] = new String[31];
-        days[0] = 8;
-        days[1] = 8;
-        days[2] = 8;
-        days[3] = 8;
-        days[4] = 8;
-        days[5] = 0;
-        days[6] = 0;
-        days[7] = 8;
-        days[8] = 8;
-        days[9] = 8;
-        days[10] = 8;
-        days[11] = 8;
-        days[12] = 0;
-        days[13] = 0;
-        days[14] = 8;
-        days[15] = 8;
-        days[16] = 8;
-        days[17] = 8;
-        days[18] = 8;
-        days[19] = 0;
-        days[20] = 0;
-        days[21] = 8;
-        days[22] = 8;
-        days[23] = 8;
-        days[24] = 8;
-        days[25] = 8;
-        days[26] = 0;
-        days[27] = 0;
-        days[28] = 8;
-        days[29] = 8;
-        days[30] = 8;
-        System.out.println(days[28]);
-
-        */
-
+     
     }
 }
