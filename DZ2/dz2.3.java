46095256ce17b56514119108f434f740fcead932
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-02-16 16:01:39 +0000

    Update dz2.3.java

--- DZ2/dz2.3.java
+++ DZ2/dz2.3.java
@@ -5,14 +5,18 @@ public class Main {
         System.out.println("Введіть кількість робочих годин на тиждень");
         Scanner scan = new Scanner(System.in);
         double a = scan.nextInt();
+        
         System.out.println("Введіть річну зарплатню з податками ");
         Scanner scan2 = new Scanner(System.in);
         double b = scan.nextInt();
+        
         System.out.println("Введіть вісоток податку ");
         Scanner scan3 = new Scanner(System.in);
         double s = scan.nextInt();
+        
         double y = (b + (b * (s / 100)));
         double d = (y / (a * 4 * 12));
+        
         System.out.println("При " + a + " робочих годин на тиждень з річною зарплатею в " + b + "" +
                 " та податком " + s + " % година часу коштує " + d);
     }
