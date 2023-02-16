ce2f1e3d8a11a860f3c0691dcc8d0f597dcc6ba7
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-02-16 15:52:11 +0000

    Update and rename Main (8).java to dz3.1.java

--- DZ3/dz3.1.java
+++ DZ3/dz3.1.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void chislo(String[] args) {
         System.out.println("Введіть число");
         Scanner scan = new Scanner(System.in);
         int chislo = scan.nextInt();
