4360b597872d426b40478a739d140ecc203dbc03
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-03-19 20:26:23 +0000

    Add files via upload

---
+++ dz11/Main.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введіть речення ->");
+        String сhars =scan.nextLine();
+        System.out.println("Кільцість символів - " + сhars.length());
+    }
+}
