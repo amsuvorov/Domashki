21134673ddedbf29b2d6f38d63de86a1f345ab25
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-03-20 18:43:21 +0000

    Update Main.java

--- dz11/Main.java
+++ dz11/Main.java
@@ -1,13 +1,32 @@
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        System.out.println("Введи речення:");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введіть речення ->");
-        String сhars =scan.nextLine();
-        System.out.println("Кільцість символів - " + сhars.length());
+        Map<Character, Integer> letterCountMap = new HashMap<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+
+            char c = sentence.charAt(i);
+
+            if (Character.isLetter(c)) {
+                Integer count = letterCountMap.get(c);
+                if (count == null) {
+                    letterCountMap.put(c, 1);
+                } else {
+                    letterCountMap.put(c, count + 1);
+                }
+            }
+        }
+        
+        System.out.println("Кількість кожної літери у реченні:");
+        for (Character letter : letterCountMap.keySet()) {
+            System.out.println(letter + ": " + letterCountMap.get(letter));
+        }
     }
 }
