ab59890c5e23f6917fee5a9196487517b0926f5a
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-04-12 15:43:56 +0100

    Update SeleniumExample.java

--- dz_16/src/main/java/org/example/SeleniumExample.java
+++ dz_16/src/main/java/org/example/SeleniumExample.java
@@ -1,16 +1,17 @@
 package org.example;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class SeleniumExample {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+       driver.manage().window().setSize(new Dimension(1920,1080));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://ithillel.ua/");
 
         WebElement programCourses = driver.findElement(By.xpath("/html/body/div[1]/main/section[2]/div/div/div[1]/div/ul/li[1]/a/div"));
@@ -18,18 +19,16 @@ public class SeleniumExample {
 
         WebElement frontEndBasic = driver.findElement(By.xpath("/html/body/div[1]/main/section[1]/div[3]/div/ul/li[1]/div/div[1]/ul/li[1]/a/div[2]"));
         frontEndBasic.click();
+        JavascriptExecutor jse= (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,2700)");
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebElement instructorsList = driver.findElement(By.xpath("/html/body/div[2]/main/section[6]/div/div/ul"));
-        List<WebElement> instructorElements = instructorsList.findElements(By.xpath("/html/body/div[1]/main/section[6]/div/div/ul/li[1]/a/div/div[2]/p[1]"));
-        for (WebElement instructor : instructorElements) {
-            String instructorName = instructor.getText();
-            System.out.println("Instructor: " + instructorName);
-        }
+        WebElement showAll = driver.findElement(By.xpath("/html/body/div[1]/main/section[6]/div/div/button"));
+        showAll.click();
+        List<WebElement> instructors = driver.findElements(By.xpath("//*[@id=\"coachesSection\"]/div/div/ul"));
 
+        for (WebElement instructor : instructors) {
+            System.out.println(instructor.getText());
+        }
+        driver.quit();
+        }
     }
-}
