70b3a84ee2c7dfc4a723b9b51e2a39c7c1e39dcc
amsuvorov <125554034+amsuvorov@users.noreply.github.com>
2023-05-26 16:47:49 +0100

    Update Main.java

--- dz15peredelka/src/main/java/org/example/Main.java
+++ dz15peredelka/src/main/java/org/example/Main.java
@@ -7,14 +7,15 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static  void main(String[] args) {
     WebDriver driver = new ChromeDriver();
-    
+
         driver.get("https://ithillel.ua/courses/testing");
     TestingPage testingPage = new TestingPage(driver);
         System.out.println(testingPage.getCourses().getText());
         System.out.println(testingPage.getOpportunities().getText());
         System.out.println(testingPage.getAdditionalCourses().getText());
-        testingPage.goToCategory("Programing").click();
-        driver.quit();
+        testingPage.goToCategory("management");
+       
+       driver.quit();
 
 }
 }
